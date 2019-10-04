571cf8816ff2d32a99d8c3e02190bde4c90c8bcc
ryota misumi <3delta7@gmail.com>
2019-10-05 04:51:42 +0900

    modify deployscript

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 heroku buildpacks:set https://github.com/heroku/heroku-buildpack-multi
 heroku config:add LD_LIBRARY_PATH=/app/.linuxbrew/lib
 heroku config:set MECAB_PATH=/app/.linuxbrew/lib/libmecab.so
+git push heroku master
 heroku run rake db:migrate
