e747e45f4b4c7a16e0d170aaf61532aabbd76ec1
Uuya <a1b23cd@icloud.com>
2019-10-05 11:31:33 +0900

    text fieldからtext areaに変更

--- db/schema.rb
+++ db/schema.rb
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2019_10_04_174613) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["login_id"], name: "index_users_on_login_id", unique: true
   end
 
 end
